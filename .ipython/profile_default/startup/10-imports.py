b916ea9a1aced69f2a30c8de7987d36dad3b37a4
sugarraysam <samuel.blaisdowdy@protonmail.com>
2020-11-15 10:26:31 -0600

    added core library from jupyter coursera environment to enable local development

---
+++ .ipython/profile_default/startup/10-imports.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import time
+
+# core library
+from core.rlglue.rl_glue import RLGlue
+from core import main_agent
+from core import ten_arm_env
+from core import test_env
---
+++ Makefile
@@ -0,0 +1,11 @@
+export IPYTHONDIR = $(PWD)/.ipython
+export PIPENV_VENV_IN_PROJECT=true
+
+.PHONY: pipenv
+pipenv:
+	@pipenv install --dev
+	@pipenv clean
+
+.PHONY: ipython
+ipython:
+	@pipenv run ipython
